cbcc13a28a83acf12f48364ceeb2f59c5d993c3a
llucoliveras <oliveraslluc@gmail.com>
2025-08-08 17:43:37 +0200

    Added Page for Testing

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
 				<Route path="/contact" element={<h1>Contact Page</h1>} />
 				<Route path="/settings" element={<h1>Settings Page</h1>} />
 				<Route path="/profile" element={<Profile />} />
+				<Route path="/test" element={<Test />} />
 				<Route path="*" element={<h1>404 Not Found</h1>} />
 			</Routes>
 		</Router>
---
+++ src/pages/Test.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { DragProvider } from "../components/DragContext";
+import DragNDropList from "../components/DragNDropList";
+
+
+
+const Test = () => {
+    const instruments = [
+        {id: "1", name: "Caixa"},
+        {id: "2", name: "Tamborí"},
+        {id: "3", name: "Surdo"},
+        {id: "4", name: "Campana"},
+        {id: "5", name: "Repenique"},
+        {id: "6", name: "Shequere"}
+    ];
+    const [editableUsers, setEditableUsers] = useState([
+        {iduser: 1, name: "Lluc", instruments: [instruments[5], instruments[3]]},
+        {iduser: 2, name: "Isaac", instruments: [instruments[1], instruments[2]]},
+        {iduser: 3, name: "Marina", instruments: [instruments[4], instruments[0]]}
+    ]);
+
+    const updateUserInstruments = (index, newInstruments) => {
+        setEditableUsers(prev => {
+            const newUsers = [...prev];
+            newUsers[index] = {
+                ...newUsers[index],
+                instruments: newInstruments,
+            };
+            return newUsers;
+        });
+    };
+
+    return (
+        <DragProvider>
+            <div>
+                {/* <div>
+                    {instruments.map((inst) => (
+                        <div
+                            key={inst.id}
+                            draggable
+                            onDragStart={() => handleOnDrag(inst)}
+                            style={{ padding: "4px", border: "1px solid gray", margin: "2px", cursor: "grab" }}
+                        >
+                            {inst.name}
+                        </div>
+                    ))}
+                </div> */}
+
+                {editableUsers.map((user, index) => 
+                    <DragNDropList
+                        key={user.iduser}
+                        listId={`user-${user.iduser}`}
+                        listItems={user.instruments}
+                        onChange={(newInstruments) => updateUserInstruments(index, newInstruments)}
+                    />
+                )}
+            </div>
+        </DragProvider>
+    );
+};
+
+export default Test;
